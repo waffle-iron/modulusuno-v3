5cd0ee47af618d9df9723d435d71e0fcee203c31
says33 <says.rodriguez@gmail.com>
2016-11-24 09:40:05 -0600

    #9 se ajusta la logica para la creacion de busines entities

--- core/grails-app/services/com/modulus/uno/BusinessEntityService.groovy
+++ core/grails-app/services/com/modulus/uno/BusinessEntityService.groovy
@@ -8,11 +8,42 @@ class BusinessEntityService {
   def clientService
   def providerService
   def employeeService
+  def bankAccountService
 
-  def appendNamesToBusinessEntity(BusinessEntity businessEntity, String[] names) {
-    def name = new ComposeName(value:names[0], type:NameType.NOMBRE)
-    def lastName = new ComposeName(value:names[1], type:NameType.APELLIDO_PATERNO)
-    def motherLastName = new ComposeName(value:names[2], type:NameType.APELLIDO_MATERNO)
+  def generatedBussinessEntityProperties(BusinessEntity businessEntity, def params, Company company) {
+    LeadType leadType = LeadType."${params.clientProviderType}"
+    if(params.persona == 'fisica'){
+      appendNamesToBusinessEntity(businessEntity, (String[])[params.name, params.lastName, params.motherLastName,params.numeroEmpleado])
+    } else {
+      appendDataToBusinessEntity(businessEntity, params.businessName)
+    }
+
+    if (params.numeroEmpleado && params.banco && params.clabe && params.cuenta){
+      Map propertie = ["clabe":params.clabe]
+      createBankAccountAndAddToBusinesEntity(propertie,businessEntity)
+    }
+
+    if((leadType == LeadType.CLIENTE || leadType == LeadType.CLIENTE_PROVEEDOR) && businessEntity.id){
+      clientService.addClientToCompany(businessEntity, company)
+    }
+    if((leadType == LeadType.PROVEEDOR || leadType == LeadType.CLIENTE_PROVEEDOR) && businessEntity.id){
+      providerService.addProviderToCompany(businessEntity, company)
+    }
+    if(leadType == LeadType.EMPLEADO){
+      businessEntity.website = null
+      employeeService.addEmployeeToCompany(businessEntity, company,params.curp)
+    }
+
+  }
+
+  def appendNamesToBusinessEntity(BusinessEntity businessEntity, String[] properties) {
+    def name = new ComposeName(value:properties[0], type:NameType.NOMBRE)
+    def lastName = new ComposeName(value:properties[1], type:NameType.APELLIDO_PATERNO)
+    def motherLastName = new ComposeName(value:properties[2], type:NameType.APELLIDO_MATERNO)
+    if (properties.length > 3) {
+      if (properties[3])
+        businessEntity.addToNames(new ComposeName(value:properties[3], type:NameType.NUMERO_EMPLEADO))
+    }
     businessEntity.addToNames(name)
     businessEntity.addToNames(lastName)
     businessEntity.addToNames(motherLastName)
@@ -26,6 +57,14 @@ class BusinessEntityService {
     businessEntity
   }
 
+  def createBankAccountAndAddToBusinesEntity(Map properties,BusinessEntity businessEntity) {
+    def bankAccountCommand = bankAccountService.createABankAccountCommandByParams(properties)
+    def bankAccount = bankAccountService.createABankAccount(bankAccountCommand)
+    bankAccount.save()
+    businessEntity.addToBanksAccounts(bankAccount)
+    businessEntity.save()
+  }
+
   def createAddressForBusinessEntity(Address address, Long businessEntityId) {
     def businessEntity = BusinessEntity.get(businessEntityId)
     businessEntity.addToAddresses(address)
@@ -33,17 +72,44 @@ class BusinessEntityService {
     businessEntity
   }
 
-  def findBusinessEntityAndProviderLinkByRFC(String rfc){
-    def businessEntity =  BusinessEntity.findByRfc(rfc)
-    if(businessEntity)
-      return ProviderLink.findByProviderRef(businessEntity.rfc)
-    businessEntity
-
+  def deleteLinksForRfc(String rfc){
+    def clientLink = ClientLink.findByClientRef(rfc)
+    clientLink?.delete()
+    def providerLink = ProviderLink.findByProviderRef(rfc)
+    providerLink?.delete()
   }
 
-  def knowIfBusinessEntityHaveABankAccountOrAddress(def rfc) {
-    def businessEntity = BusinessEntity.findByRfc(rfc)
-    [businessEntity.banksAccounts,businessEntity.addresses]
+  def findBusinessEntityByKeyword(String keyword, String entity, Company company){
+    if (!entity)
+      entity=""
+    def criteria = BusinessEntity.createCriteria()
+    def list = criteria.listDistinct {
+      or{
+        like 'rfc', "%${keyword}%"
+        names{
+          like 'value', "%${keyword}%"
+        }
+      }
+
+    }
+    list.collect {
+      if ((entity.equals("CLIENT") && clientService.isClientOfThisCompany(it, company))
+          || (entity.equals("PROVIDER") && providerService.isProviderOfThisCompany(it, company))
+          || (entity.equals("EMPLOYEE") && employeeService.isEmployeeOfThisCompany(it, company))
+          || (entity.equals("") && (clientService.isClientOfThisCompany(it, company) || providerService.isProviderOfThisCompany(it, company) || employeeService.isEmployeeOfThisCompany(it, company)))
+      )
+        it
+    }.findResults{it}.unique()
+  }
+
+  def getClientProviderType(String rfc){
+    def providerLink = ProviderLink.findByProviderRef(rfc)
+    def clientLink = ClientLink.findByClientRef(rfc)
+    def employeeLink = EmployeeLink.findByEmployeeRef(rfc)
+    if(providerLink && clientLink) return LeadType.CLIENTE_PROVEEDOR
+      if(clientLink) return LeadType.CLIENTE
+        if(providerLink) return LeadType.PROVEEDOR
+          if (employeeLink) return LeadType.EMPLEADO
   }
 
   def updateNamesToBusinessEntity(BusinessEntity businessEntity, String[] names) {
@@ -76,70 +142,27 @@ class BusinessEntityService {
     businessEntity
   }
 
-  def getClientProviderType(String rfc){
-    def providerLink = ProviderLink.findByProviderRef(rfc)
-    def clientLink = ClientLink.findByClientRef(rfc)
-    def employeeLink = EmployeeLink.findByEmployeeRef(rfc)
-    if(providerLink && clientLink) return LeadType.CLIENTE_PROVEEDOR
-      if(clientLink) return LeadType.CLIENTE
-        if(providerLink) return LeadType.PROVEEDOR
-          if (employeeLink) return LeadType.EMPLEADO
+  def findBusinessEntityAndProviderLinkByRFC(String rfc){
+    def businessEntity =  BusinessEntity.findByRfc(rfc)
+    if(businessEntity)
+      return ProviderLink.findByProviderRef(businessEntity.rfc)
+      businessEntity
+
   }
 
-  def deleteLinksForRfc(String rfc){
-    def clientLink = ClientLink.findByClientRef(rfc)
-    clientLink?.delete()
-    def providerLink = ProviderLink.findByProviderRef(rfc)
-    providerLink?.delete()
-  }
-
-  def findBusinessEntityByKeyword(String keyword, String entity, Company company){
-    def tokens = keyword ? keyword.tokenize() : ""
-
-    def criteria = BusinessEntity.createCriteria()
-    def list = criteria.listDistinct {
-      or{
-        ilike 'rfc', "%${keyword}%"
-        names{
-          or {
-            'in'("value", tokens)
-            ilike "value", "%${keyword}%"
-          }
-        }
-      }
-    }
-
-    def leadType = getLeadTypeOfEntity(entity)
-
-    def listOfCompany = company.businessEntities.collect {
-      if (getClientProviderType(it.rfc).toString().contains(leadType.toString()) && list.contains(it)){
-        it
-      }
-    }.findResults{it}.unique()
-
-    listOfCompany
-  }
-
-  private def getLeadTypeOfEntity(String entity) {
-    switch (entity) {
-      case "CLIENT": LeadType.CLIENTE
-        break
-      case "PROVIDER": LeadType.PROVEEDOR
-        break
-      case "EMPLOYEE": LeadType.EMPLEADO
-        break
-      default: null
-        break
-    }
+  def knowIfBusinessEntityHaveABankAccountOrAddress(def rfc) {
+    def businessEntity = BusinessEntity.findByRfc(rfc)
+    [businessEntity.banksAccounts,businessEntity.addresses]
   }
 
   def generateSubAccountStp(Company company, BusinessEntity businessEntity) {
-    ClientLink client = ClientLink.findByCompanyAndClientRef(company, businessEntity.rfc)
-    client = clientService.generateSubAccountStp(client, businessEntity)
-    client
-  }
+  ClientLink client = ClientLink.findByCompanyAndClientRef(company, businessEntity.rfc)
+  client = clientService.generateSubAccountStp(client, businessEntity)
+  client
+}
 
-  ClientLink getClientLinkOfBusinessEntityAndCompany(BusinessEntity businessEntity, Company company) {
-    ClientLink.findByCompanyAndClientRef(company, businessEntity.rfc)
-  }
+ClientLink getClientLinkOfBusinessEntityAndCompany(BusinessEntity businessEntity, Company company) {
+  ClientLink.findByCompanyAndClientRef(company, businessEntity.rfc)
+}
+
 }
