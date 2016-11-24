de169238eac4846afebd2ef16cbf7a127543dbe7
says33 <says.rodriguez@gmail.com>
2016-11-24 15:15:38 -0600

    #3 se ajusta el log de la aplicacion para mostrarlo adecuadamente·

--- webservices/grails-app/conf/logback.groovy
+++ webservices/grails-app/conf/logback.groovy
@@ -1,23 +1,38 @@
 import grails.util.BuildSettings
 import grails.util.Environment
+import org.springframework.boot.ApplicationPid
+import java.nio.charset.Charset
+import java.text.SimpleDateFormat
 
-// See http://logback.qos.ch/manual/groovy.html for details on configuration
-appender('STDOUT', ConsoleAppender) {
-    encoder(PatternLayoutEncoder) {
-        pattern = "%level %logger - %msg%n"
-    }
+if (!System.getProperty("PID")) {
+    System.setProperty("PID", (new ApplicationPid()).toString())
 }
 
-root(ERROR, ['STDOUT'])
+def basePath = System.getenv("CATALINA_BASE") ?: "."
 
-def targetDir = BuildSettings.TARGET_DIR
-if (Environment.isDevelopmentMode() && targetDir) {
-    appender("FULL_STACKTRACE", FileAppender) {
-        file = "${targetDir}/stacktrace.log"
-        append = true
-        encoder(PatternLayoutEncoder) {
-            pattern = "%level %logger - %msg%n"
-        }
-    }
-    logger("StackTrace", ERROR, ['FULL_STACKTRACE'], false)
+conversionRule 'clr', org.springframework.boot.logging.logback.ColorConverter
+conversionRule 'wex', org.springframework.boot.logging.logback.WhitespaceThrowableProxyConverter
+
+def bySecond = timestamp("yyyyMMdd'T'HHmmss")
+
+appender('ROLLING',RollingFileAppender) {
+  encoder(PatternLayoutEncoder){
+    charset = Charset.forName('UTF-8')
+    pattern =
+      '%clr(%d{yyyy-MM-dd HH:mm:ss.SSS}){faint} ' + // Date
+      '%clr(%5p) ' + // Log level
+      '%clr(%property{PID}){magenta} ' + // PID
+      '%clr(---){faint} %clr([%15.15t]){faint} ' + // Thread
+      '%clr(%-40.40logger{39}){cyan} %clr(:){faint} ' + // Logger
+      '%m%n%wex' // Message
+  }
+   rollingPolicy(TimeBasedRollingPolicy){
+    FileNamePattern = "${basePath}/logs/modulusuno-%d{yyyy-MM}.log"
+  }
+
 }
+
+logger 'grails.artefact.Interceptor', DEBUG, ['ROLLING'], false
+logger 'grails.app.controllers', DEBUG, ['ROLLING'], false
+logger('org.springframework', WARN)
+root(WARN, ['ROLLING'])
