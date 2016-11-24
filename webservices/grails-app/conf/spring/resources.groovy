349527eaa0b2b723a86afe1dbb3de8ba28928c65
says33 <says.rodriguez@gmail.com>
2016-11-24 15:16:18 -0600

    #3 se agrega la configuracion de beans necesarios para resolucion de llamadas rest

--- webservices/grails-app/conf/spring/resources.groovy
+++ webservices/grails-app/conf/spring/resources.groovy
@@ -1,3 +1,15 @@
 // Place your Spring DSL code here
+
+
+import groovyx.net.http.RESTClient
 beans = {
+  localeResolver(org.springframework.web.servlet.i18n.SessionLocaleResolver) {
+    defaultLocale = new Locale("es","ES_MX")
+    java.util.Locale.setDefault(defaultLocale)
+  }
+
+  restClientBean(RESTClient) {
+    // uri = grailsApplication.config.modulus.url
+  }
+
 }
