27ee8e61f8d95606648358810ff38620a0162530
says33 <says.rodriguez@gmail.com>
2016-11-24 15:23:23 -0600

    #3 se agrega la configuracion de el metadata de swagger

---
+++ webservices/grails-app/conf/application.groovy
@@ -0,0 +1,9 @@
+swaggydoc {
+  contact = "sergio.rodriguez@techminds.com.mx"
+  description = """
+  |Swagger Api Modulus UNO ImpulsoMX """.stripMargin()
+  license = "Apache 2.0"
+  licenseUrl = "http://www.apache.org/licenses/LICENSE-2.0.html"
+  title = "Swagger Api"
+  apiVersion = "1.0"
+}
