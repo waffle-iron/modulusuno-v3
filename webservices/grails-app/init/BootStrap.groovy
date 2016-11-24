97f72b721798e50c466ee80f26df971e4c48c4dc
says33 <says.rodriguez@gmail.com>
2016-11-24 15:21:49 -0600

    #3 se agrega la invocacion del mapeo de lso marshallers e las clases dominio

--- webservices/grails-app/init/BootStrap.groovy
+++ webservices/grails-app/init/BootStrap.groovy
@@ -1,7 +1,16 @@
 class BootStrap {
 
+    def grailsApplication
+
     def init = { servletContext ->
+      grailsApplication.getArtefacts("Domain").each{dc->
+        //here we register custom marshallers declared in domain classes
+        if(dc.hasProperty("marshaller")){
+          dc.clazz.marshaller()
+        }
+      }
     }
+
     def destroy = {
     }
 }
