539022cd794bfe20eabf10564a299616f2b07b3d
temocpalma <temopalmag@gmail.com>
2016-12-01 11:34:38 -0600

    #12 Agregando taglib para descargar el xml de acuse de cancelación

--- web/grails-app/taglib/com/modulus/uno/SaleOrderTagLib.groovy
+++ web/grails-app/taglib/com/modulus/uno/SaleOrderTagLib.groovy
@@ -14,10 +14,21 @@ class SaleOrderTagLib {
     out << "${grailsApplication.config.modulus.facturacionUrl}${createUrlToShowFile(attrs)}"
   }
 
+  def invoiceAccuseUrl = { attrs, body ->
+    out << "${grailsApplication.config.modulus.facturacionUrl}${createUrlToShowAccuse(attrs)}"
+  }
+
   private def createUrlToShowFile(def attrs) {
     def file = "${attrs.saleOrder.uuid}_${attrs.saleOrder.folio}.${attrs.format}"
     def rfc = "${attrs.saleOrder.company.rfc}"
     def url = grailsApplication.config.modulus.showFactura
+    url.replace('#rfc',rfc).replace('#file',file)
+  }
+
+  private def createUrlToShowAccuse(def attrs) {
+    def file = "${attrs.saleOrder.uuid}.xml"
+    def rfc = "${attrs.saleOrder.company.rfc}"
+    def url = grailsApplication.config.modulus.showAccuse
     url.replace('#rfc',rfc).replace('#file',file)
   }
 
