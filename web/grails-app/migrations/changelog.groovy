8fc3fdfa151f20732eef59bd0530c3cfdb1f47b7
says33 <says.rodriguez@gmail.com>
2016-12-07 12:28:52 -0600

    #25 se agrega las referencias de los changesets al changelog para la actualizacion de la base de datos

--- web/grails-app/migrations/changelog.groovy
+++ web/grails-app/migrations/changelog.groovy
@@ -18,4 +18,6 @@ databaseChangeLog = {
     include file: 'adding-ids-of-artimisa.groovy'
     include file: 'adding-date-operation-in-sale-and-puchase-order.groovy'
     include file: 'creating-role-ejecutor.groovy'
+    include file: 'adding-new-entity-banking-transactions.groovy'
+    include file: 'delete-constraint-reference-in-movimientos-bancarios.groovy'
 }
