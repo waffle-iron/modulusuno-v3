e6594c73ecf2f4654a403933a6ce804f5926e607
says33 <says.rodriguez@gmail.com>
2016-12-12 12:22:07 -0600

    #5 se crea el service para la creacion de crporates, agregado de companies y users, asi como a cada user agregado se le agregara elk role de corporative

---
+++ core/grails-app/services/com/modulus/uno/CorporateService.groovy
@@ -0,0 +1,45 @@
+package com.modulus.uno
+
+import grails.transaction.Transactional
+
+@Transactional
+class CorporateService {
+
+  def addCompanyToCorporate(Corporate corporate, Company company) {
+    corporate.addToCompanies(company)
+    corporate.save()
+    corporate
+  }
+
+  def addUserToCorporate(Corporate corporate, User user) {
+    corporate.addToUsers(user)
+    corporate.save()
+    setAuthorityToUser(user)
+    corporate
+  }
+
+  def createNewCorporate(String name, String url) {
+    Corporate corporate = new Corporate(nameCorporate:name, corporateUrl:url).save()
+    corporate
+  }
+
+
+  def createRoute53(Corporate corporate) {
+    //checar esta ligas http://docs.aws.amazon.com/cli/latest/userguide/cli-chap-getting-started.html
+    true
+  }
+
+
+  def createAVirtualHostNginx(Corporate corporate) {
+    //Esta en si en manejo del nginx pero deveras de chacar muy bien los puestos que se van usando tanto para cada nuevo coorporativo
+    true
+  }
+
+  private def setAuthorityToUser(User user) {
+    def userRole = Role.findWhere(authority: 'ROLE_CORPORATIVE')
+    UserRole.create user, userRole, true
+    user.save()
+  }
+
+
+}
