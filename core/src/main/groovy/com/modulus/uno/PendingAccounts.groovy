b4adc816418797f4542f041d225fe0e139f13a6b
temocpalma <temopalmag@gmail.com>
2016-12-14 13:23:41 -0600

    #36 Agregando los atributos para los totales vencidos en el bean de cuentas por cobrar/pagar

--- core/src/main/groovy/com/modulus/uno/PendingAccounts.groovy
+++ core/src/main/groovy/com/modulus/uno/PendingAccounts.groovy
@@ -3,6 +3,9 @@ package com.modulus.uno
 class PendingAccounts {
   BigDecimal totalCharges
   BigDecimal totalPayments
+  BigDecimal totalExpiredCharges
+  BigDecimal totalExpiredPayments
+
   List<SaleOrder> listCharges
   List<PurchaseOrder> listPayments
   Date startDate
