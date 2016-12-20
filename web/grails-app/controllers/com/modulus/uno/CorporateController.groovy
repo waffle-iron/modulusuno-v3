3ebaab2ea7f63f5a3df1b6727a26d8f137ea9e8c
egjimenezg <egjimenezg@gmail.com>
2016-12-20 16:56:41 -0600

    coding #5 save action was added to corporate controller

--- web/grails-app/controllers/com/modulus/uno/CorporateController.groovy
+++ web/grails-app/controllers/com/modulus/uno/CorporateController.groovy
@@ -1,9 +1,34 @@
 package com.modulus.uno
+import grails.transaction.Transactional
 
+
+@Transactional(readOnly = true)
 class CorporateController {
+
+  CorporateService corporateService
 
   def create(){
     respond new Corporate(params)
+  }
+
+  @Transactional
+  def save(Corporate corporate){
+    if(!corporate){
+      transactionStatus.setRollbackOnly()
+      respond corporate.errors, view:'create'
+      return
+    }
+    def user = springSecurityService.currentUser
+    corporateService.saveNewCorporate(corporate,user)
+
+    request.withFormat {
+      form multipartForm {
+        flash.message = message(code:'default.created.message', args:[message(code: 'corporate.label',
+                                                                              default: 'Corporate'),corporate.id])
+        redirect corporate
+      }
+      '*' { respond corporate, [status:CREATED] }
+    }
   }
 
 }
