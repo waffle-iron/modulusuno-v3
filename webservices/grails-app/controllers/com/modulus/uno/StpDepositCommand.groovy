bb86b923cc17c5095dd8a464d20575161030d0df
temocpalma <temopalmag@gmail.com>
2016-12-22 14:27:49 -0600

    #29 Agregando campos del command del deposito STP

--- webservices/grails-app/controllers/com/modulus/uno/StpDepositCommand.groovy
+++ webservices/grails-app/controllers/com/modulus/uno/StpDepositCommand.groovy
@@ -2,4 +2,18 @@ package com.modulus.uno
 import grails.validation.Validateable
 
 class StpDepositCommand implements Validateable {
+  String clave
+  String fechaOperacion
+  String institucionOperante
+  String institucionBeneficiaria
+  String claveRastreo
+  String monto
+  String nombreOrdenante
+  String nombreBeneficiario
+  String tipoCuentaBeneficiario
+  String cuentaBeneficiario
+  String rfcCurpCuentaBeneficiario
+  String conceptoPago
+  String referenciaNumerica
+  String empresa
 }
