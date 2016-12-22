272ccf19b2b81ba7d1233293c8fac036ef12950c
temocpalma <temopalmag@gmail.com>
2016-12-22 14:28:18 -0600

    #29 Agregando domain de los avisos de depósito en cuentas de STP

---
+++ core/grails-app/domain/com/modulus/uno/StpDeposit.groovy
@@ -0,0 +1,18 @@
+package com.modulus.uno
+
+class StpDeposit {
+  Long operationNumber
+  Date operationDate
+  String payerKey
+  String beneficiaryKey
+  String tracingKey
+  BigDecimal amount
+  String payerName
+  String beneficiaryName
+  Long typeAccountBeneficiary
+  String accountBeneficiary
+  String rfcCurpBeneficiary
+  String paymentConcept
+  Long numericalReference
+  String companyNameStp
+}
