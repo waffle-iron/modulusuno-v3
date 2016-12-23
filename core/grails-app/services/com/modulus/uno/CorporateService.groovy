7c47689129bf506c7984d09be4e87d9f8d94758b
egjimenezg <egjimenezg@gmail.com>
2016-12-23 10:21:40 -0600

    coding #7 updating the method to add a new user

--- core/grails-app/services/com/modulus/uno/CorporateService.groovy
+++ core/grails-app/services/com/modulus/uno/CorporateService.groovy
@@ -5,6 +5,9 @@ import grails.transaction.Transactional
 @Transactional
 class CorporateService {
 
+  RecoveryService recoveryService
+  UserService userService
+
   def addCompanyToCorporate(Corporate corporate, Company company) {
     corporate.addToCompanies(company)
     corporate.save()
@@ -12,10 +15,12 @@ class CorporateService {
   }
 
   def addUserToCorporate(Long corporateId, User user) {
+    User createdUser = userService.createUserWithoutRole(user,user.profile)
+    Role role = Role.findWhere(authority: 'ROLE_CORPORATIVE')
+    createdUser = userService.setAuthorityToUser(createdUser,role)
     Corporate corporate = Corporate.get(corporateId)
-    corporate.addToUsers(user)
+    corporate.addToUsers(createdUser)
     corporate.save()
-    setAuthorityToUser(user)
     corporate
   }
 
@@ -42,7 +47,7 @@ class CorporateService {
     }
   }
 
-  private def setAuthorityToUser(User user) {
+  private User setAuthorityToUser(def user) {
     def userRole = Role.findWhere(authority: 'ROLE_CORPORATIVE')
     UserRole.create user, userRole, true
     user.save()
