b118b7c961c00bc437d4fdb07306e5e503fbd2f7
cggg88jorge <jorge@makingdevs.com>
2016-12-26 10:15:09 -0600

    #8 ajustando el servicio aca bien chingo con el principe del desarrollo

--- core/grails-app/services/com/modulus/uno/OrganizationService.groovy
+++ core/grails-app/services/com/modulus/uno/OrganizationService.groovy
@@ -11,9 +11,10 @@ class OrganizationService {
     User user = User.findByUsername(username)
     rolesForCompanies.each { companyName, rolesForThisCompany ->
       Company company = Company.findByBussinessName(companyName)
-      rolesForThisCompany.each { role, value ->
-        roleService.createRoleForUserAtThisCompany(role, user, company)
+      List<Role> roles = rolesForThisCompany.collect { role, value ->
+        Role.findByAuthority(role)
       }
+      roleService.createRolesForUserAtThisCompany(roles, user, company)
     }
     user
   }
