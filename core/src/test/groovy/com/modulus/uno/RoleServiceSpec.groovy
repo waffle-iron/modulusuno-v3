dc709aa4019031ab44d7334ad197772ad603d175
cggg88jorge <jorge@makingdevs.com>
2016-12-26 11:27:26 -0600

    #8 creando la prueba para la creacion de roles por usuario en una compañia

--- core/src/test/groovy/com/modulus/uno/RoleServiceSpec.groovy
+++ core/src/test/groovy/com/modulus/uno/RoleServiceSpec.groovy
@@ -2,21 +2,28 @@ package com.modulus.uno
 
 import grails.test.mixin.TestFor
 import spock.lang.Specification
+import java.lang.Void as Should
+import grails.test.mixin.Mock
 
-/**
- * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
- */
 @TestFor(RoleService)
+@Mock([User, Company, Role, UserRoleCompany])
 class RoleServiceSpec extends Specification {
 
-    def setup() {
-    }
+  Should "Create the roles for user at one company"(){
+    given:
+      List<Role> roles = ["ROLE_LEGAL_REPRESENTATIVE_EJECUTOR","ROLE_FICO_VISOR"].collect { String r ->
+        new Role(r).save(validate:false)
+      }
+      Company company = new Company(bussinessName:"makingdevs").save(validate:false)
+      User user = new User("user","user")
+      user.save(validate:false)
+    when:
+      UserRoleCompany userRoleByCompany = service.createRolesForUserAtThisCompany(roles,user,company)
+    then:
+      userRoleByCompany.id
+      userRoleByCompany.company == company
+      userRoleByCompany.user == user
+      userRoleByCompany.roles.size() == 2
+  }
 
-    def cleanup() {
-    }
-
-    void "test something"() {
-        expect:"fix me"
-            true == false
-    }
 }
