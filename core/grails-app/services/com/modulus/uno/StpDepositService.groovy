bfd43df27e8ed03f32a694ae6653f5a97849cc85
temocpalma <temopalmag@gmail.com>
2016-12-27 10:32:55 -0600

    #29 Corrigiendo creación del commanden el método que guarda la notificación de depósito stp

--- core/grails-app/services/com/modulus/uno/StpDepositService.groovy
+++ core/grails-app/services/com/modulus/uno/StpDepositService.groovy
@@ -8,29 +8,36 @@ class StpDepositService {
 
   def notificationDepositFromStp(String xmlNotif) {
     def notification = new XmlSlurper().parseText(xmlNotif)
+    log.info "Notification: ${notification}"
+
     StpDepositCommand command = new StpDepositCommand(
       clave:notification.Clave,
       fechaOperacion:notification.FechaOperacion,
-      institucionOperante:notification.InstitucionOperante.@clave,
+      institucionOrdenante:notification.InstitucionOrdenante.@clave,
       institucionBeneficiaria:notification.InstitucionBeneficiaria.@clave,
       claveRastreo:notification.ClaveRastreo,
       monto:notification.Monto,
-      nombreOrdenante:notification.NombreOrdenante,
-      tipoCuentaBeneficiario:notification.TipoCuentaBeneficiario,
+      nombreOrdenante:notification.NombreOrdenante ?: "",
+      nombreBeneficiario:notification.NombreBeneficiario,
+      tipoCuentaBeneficiario:notification.TipoCuentaBeneficiario.@clave,
       cuentaBeneficiario:notification.CuentaBeneficiario,
-      rfcCurpCuentaBeneficiario:notification.RfcCurpCuentaBeneficiario,
+      rfcCurpBeneficiario:notification.RfcCurpBeneficiario,
       conceptoPago:notification.ConceptoPago,
       referenciaNumerica:notification.ReferenciaNumerica,
       empresa:notification.Empresa
     )
-    if (!command.validate()) {
-      throw new BusinessException("Invalid parameters")
-    }
+
+    println "Command: ${command.dump()}"
+    log.info "Command: ${command.dump()}"
 
     StpDeposit stpDeposit = command.createStpDeposit()
+    println "StpDeposit: ${stpDeposit.dump()}"
+    log.info "StpDeposit: ${stpDeposit.dump()}"
+
     stpDeposit.save()
     //actualizar saldo de cuenta bancaria stp
     //registrar cashin en M1
+    stpDeposit
   }
 
 }
