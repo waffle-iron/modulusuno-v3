14611dc70e438deb7222a25303c72a55b106a122
cggg88jorge <jorge@makingdevs.com>
2016-12-26 20:14:07 -0600

    #72 agregando definicion del metodo para el borrado de roles por una sola compañia y reidentando servicio

--- core/grails-app/services/com/modulus/uno/RoleService.groovy
+++ core/grails-app/services/com/modulus/uno/RoleService.groovy
@@ -5,16 +5,21 @@ import grails.transaction.Transactional
 @Transactional
 class RoleService {
 
-    UserRoleCompany createRolesForUserAtThisCompany(List<Role> roles, User user, Company company) {
-      UserRoleCompany userRoleByCompany = new UserRoleCompany(user:user,company:company)
-      roles.collect{ Role role ->
-        userRoleByCompany.addToRoles(role)
-      }
-      userRoleByCompany.save()
-      userRoleByCompany
+  UserRoleCompany createRolesForUserAtThisCompany(List<Role> roles, User user, Company company) {
+    UserRoleCompany userRoleByCompany = new UserRoleCompany(user:user,company:company)
+    roles.collect{ Role role ->
+      userRoleByCompany.addToRoles(role)
     }
+    userRoleByCompany.save()
+    userRoleByCompany
+  }
 
-    UserRoleCompany findRolesForUserAtThisCompany(User user, Company company){
-      UserRoleCompany.findByCompanyAndUser(company, user)
-    }
+  UserRoleCompany findRolesForUserAtThisCompany(User user, Company company){
+    UserRoleCompany.findByCompanyAndUser(company, user)
+  }
+
+  def deleteRolesForUserAtThisCompany(User user, Company company){
+
+  }
+
 }
