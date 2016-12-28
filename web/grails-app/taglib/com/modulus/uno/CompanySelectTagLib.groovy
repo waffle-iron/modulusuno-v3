189835a91cf5789a9f885b48ae64c99d572e830c
cggg88jorge <jorge@makingdevs.com>
2016-12-28 11:47:30 -0600

    #76 ajustar taglib y se agrega llamda a servicio

--- web/grails-app/taglib/com/modulus/uno/CompanySelectTagLib.groovy
+++ web/grails-app/taglib/com/modulus/uno/CompanySelectTagLib.groovy
@@ -2,28 +2,26 @@ package com.modulus.uno
 
 class CompanySelectTagLib {
 
-    def companyService
-    def springSecurityService
+  def springSecurityService
+  OrganizationService organizationService
 
-    static namespace = "companyInfo"
- static defaultEncodeAs = "raw"
+  static namespace = "companyInfo"
+  static defaultEncodeAs = "raw"
 
-    //static encodeAsForTags = [tagName: [taglib:'html'], otherTagName: [taglib:'none']]
+  def companyInfo = { attrs, body ->
+    def company = Company.findById(session.company.toLong())
+    out << "${company.toString()}"
+  }
 
-    def companyInfo = { attrs, body ->
-      def company = Company.findById(session.company.toLong())
-      out << "${company.toString()}"
-    }
+  def selectedCompany = { attrs,body ->
+    def user = springSecurityService.currentUser
+    def companies = organizationService.findAllCompaniesOfUser(user)
+    out << g.select(from:companies, id:"companyNavSelect", name:"company",optionKey:"id", value:"${session.company}",required:"required")
+  }
 
-    def selectedCompany = { attrs,body ->
-      def user = springSecurityService.currentUser
-      def companies = companyService.findCompaniesForThisUser(user)
-      out << g.select(from:companies, id:"companyNavSelect", name:"company",optionKey:"id", value:"${session.company}",required:"required")
-    }
-
-    def isAvailableForOperationInThisCompany = { attrs, body ->
-      def company = Company.findById(session.company.toLong())
-      out << (company.status == CompanyStatus.ACCEPTED)
-    }
+  def isAvailableForOperationInThisCompany = { attrs, body ->
+    def company = Company.findById(session.company.toLong())
+    out << (company.status == CompanyStatus.ACCEPTED)
+  }
 
 }
