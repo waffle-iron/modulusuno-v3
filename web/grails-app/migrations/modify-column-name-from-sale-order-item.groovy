fa4d269a8242cdf60e70fb4bfa875c21101f8aae
cggg88jorge <jorge@makingdevs.com>
2017-01-19 16:29:52 -0600

    #116 quitando todos los changeset que ya no se utilizaran

--- web/grails-app/migrations/modify-column-name-from-sale-order-item.groovy
+++
@@ -1,6 +0,0 @@
-databaseChangeLog = {
-  changeSet(author:"tim (manual)", id: "growSizeNameSaleOrderItem") {
-    modifyDataType (tableName: "sale_order_item", columnName: "name", newDataType: "VARCHAR(200)")
-    addNotNullConstraint (columnDataType:"VARCHAR(200)", columnName:"name", tableName:"sale_order_item")
-  }
-}
