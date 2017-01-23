610325d5e78ef6aa329fc43cec9da728999b9b32
temocpalma <temopalmag@gmail.com>
2017-01-23 17:09:54 -0600

    #118 Refactorizando changeset que actualiza los registros existentes de órdenes de compra con el uuid

--- web/grails-app/migrations/initializeUuidToPurchaseOrder.groovy
+++ web/grails-app/migrations/initializeUuidToPurchaseOrder.groovy
@@ -1,15 +1,12 @@
-import liquibase.statement.core.*
+ import liquibase.statement.core.*
 import com.modulus.uno.PurchaseOrder
 
 databaseChangeLog = {
   changeSet(author: "temoc (manual)", id: "initializeUuidToPurchaseOrder") {
     grailsChange {
       change {
-        PurchaseOrder.withTransaction {
         PurchaseOrder.list().each {
-          it.uuid = UUID.randomUUID().toString().replace('-','')[0..15]
-          it.save(flush:true)
-        }
+          sql.execute("update purchase_order set uuid=${UUID.randomUUID().toString().replace('-','')[0..15]} where id=${it.id}")
         }
       }
     }
