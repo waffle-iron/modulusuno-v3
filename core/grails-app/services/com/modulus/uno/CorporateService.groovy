ab74ee62bddf5d7d1455da21d8891e7bab57f083
says33 <says.rodriguez@gmail.com>
2017-01-24 13:04:35 -0600

    #123 se cambia la obtencion de los archvios por medio del getResources

--- core/grails-app/services/com/modulus/uno/CorporateService.groovy
+++ core/grails-app/services/com/modulus/uno/CorporateService.groovy
@@ -8,6 +8,7 @@ class CorporateService {
   RecoveryService recoveryService
   def springSecurityService
   def awsRoute53Service
+  def grailsResourceLocator
 
   def addCompanyToCorporate(Corporate corporate, Company company) {
     corporate.addToCompanies(company)
@@ -91,8 +92,9 @@ class CorporateService {
   }
 
   private def createWebAndApiViHost(Corporate corporate, String baseUrl, String destFile) {
-    def fileDefaultWeb =  new File('src/main/groovy/com/modulus/uno/web.txt')
-    def fileDefaultApi =  new File('src/main/groovy/com/modulus/uno/api.txt')
+    def file = corporate.getClass().getClassLoader().getResource("web.txt").file
+    def fileDefaultWeb =  new File(corporate.getClass().getClassLoader().getResource("web.txt").file)
+    def fileDefaultApi =  new File(corporate.getClass().getClassLoader().getResource("api.txt").file)
     def fileNewWeb = new File("${destFile}${corporate.corporateUrl}.conf")
     def fileNewApi = new File("${destFile}api-${corporate.corporateUrl}.conf")
     copyAndReplaceTextInFile(fileDefaultWeb,fileNewWeb) { it.replaceAll('urlCorporate',"${corporate.corporateUrl}${baseUrl}" )}
