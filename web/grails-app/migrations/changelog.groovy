3f3e24f9e5699f157d08873605a428b8ffce89c3
temocpalma <temopalmag@gmail.com>
2017-01-25 09:52:39 -0600

    #124 Agregando al changelog el changeset para mover el uuid actual al campo folio y regenerando los uuids de órdenes de venta

--- web/grails-app/migrations/changelog.groovy
+++ web/grails-app/migrations/changelog.groovy
@@ -3,4 +3,5 @@ databaseChangeLog = {
   include file: 'create-m1-roles.groovy'
   include file: 'adding-user-admin-production.groovy'
   include file: 'adding-list-banks.groovy'
+  include file: 'moving-data-from-uuid-to-folio-in-sale-order.groovy'
 }
