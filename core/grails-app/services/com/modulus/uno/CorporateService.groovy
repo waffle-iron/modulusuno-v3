29d3ba49421f3fb1d4937bafd7a00d83fffa3acc
says33 <says.rodriguez@gmail.com>
2017-01-25 10:36:35 -0600

    se agrega nuevo log para verificar la creacion

--- core/grails-app/services/com/modulus/uno/CorporateService.groovy
+++ core/grails-app/services/com/modulus/uno/CorporateService.groovy
@@ -23,6 +23,8 @@ class CorporateService {
   def createRoute53(Corporate corporate) {
     def valueHost = System.env['VALUE_HOST_IP']
     def baseUrl = System.env['DOMAIN_BASE_URL']
+    log.info valueHost
+    log.info "****************************"
     awsRoute53Service.createRecordSet(corporate.corporateUrl,"-api${baseUrl}", valueHost)
     awsRoute53Service.createRecordSet(corporate.corporateUrl,baseUrl, valueHost)
     true
