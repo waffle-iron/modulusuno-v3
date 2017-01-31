f628ec9de8847a877258b0f6c94d4a208feb3c3b
says33 <says.rodriguez@gmail.com>
2017-01-31 15:57:39 -0600

    #137 se ajustan las pruebas de userService

--- core/src/test/groovy/com/modulus/uno/UserServiceSpec.groovy
+++ core/src/test/groovy/com/modulus/uno/UserServiceSpec.groovy
@@ -6,34 +6,15 @@ import grails.test.mixin.TestFor
 import java.lang.Void as Should
 
 @TestFor(UserService)
-@Mock([UserRole])
+@Mock([UserRole,Profile,User])
 class UserServiceSpec extends Specification {
 
-  def service = new UserService()
   def recoveryService = Mock(RecoveryService)
 
   def setup(){
     service.recoveryService = recoveryService
   }
 
-  void "should create an user"() {
-  given: "An user and email"
-    def user = Mock(User)
-    def email = 'josdem@email.com'
-  and: "A role"
-    def role = Mock(Role)
-  and: "A profile"
-    def profile = Mock(Profile)
-    profile.email >> email
-    user.profile >> profile
-    Profile.metaClass.static.findByEmail = { null }
-  when: "We assing values to command"
-    service.save(user, role)
-  then:"We validate command"
-    user.profile >> profile
-    1 * user.save()
-    1 * recoveryService.sendConfirmationAccountToken(email)
-  }
 
   Should "save an user without Role"(){
     given:"the user"
