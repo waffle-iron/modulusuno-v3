72c3b7ba85c293c9ef4b9dd920f1779c4d484b79
temocpalma <temopalmag@gmail.com>
2017-02-10 08:54:04 -0600

    #159 Agregando changeset que agrega el método de pago a orden de venta

---
+++ web/grails-app/migrations/adding-column-payment-method-to-sale-order.groovy
@@ -0,0 +1,20 @@
+import com.modulus.uno.SaleOrder
+
+databaseChangeLog = {
+
+    changeSet(author: "tim (generated)", id: "1486681466430-1") {
+        addColumn(tableName: "sale_order") {
+            column(name: "payment_method", type: "varchar(255)") {
+                constraints(nullable: "false")
+            }
+        }
+    }
+
+    changeSet(author: "temoc (manual)", id: "initialize-payment-method-to-existing-sale-orders") {
+      grailsChange {
+        change {
+          sql.execute("update sale_order set payment_method='03 - TRANSFERENCIA ELECTRONICA'")
+        }
+      }
+    }
+}
