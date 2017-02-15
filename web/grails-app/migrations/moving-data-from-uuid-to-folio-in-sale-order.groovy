2f705b31c38f652e09f04bb8d38021a5bb9270fd
egjimenezg <egjimenezg@gmail.com>
2017-02-15 13:08:58 -0600

    #162 removing gorm from changeSet

--- web/grails-app/migrations/moving-data-from-uuid-to-folio-in-sale-order.groovy
+++ web/grails-app/migrations/moving-data-from-uuid-to-folio-in-sale-order.groovy
@@ -1,15 +1,10 @@
-import com.modulus.uno.SaleOrder
-
 databaseChangeLog = {
 
   changeSet(author: "temoc (manual)", id: "moving-uuid-to-folio-from-sale-order") {
     grailsChange {
       change {
-        SaleOrder.list().each {
-          it.folio = it.uuid
-          it.uuid = UUID.randomUUID().toString().replace('-','')[0..15]
-          it.save()
-        }
+        sql.execute("update sale_order set folio = uuid")
+        sql.execute("update sale_order set uuid = ${UUID.randomUUID().toString().replace('-','')[0..15]}")
       }
     }
   }
