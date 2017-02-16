5000c91cee23963b6c90c29bb133175e7123b4d8
egjimenezg <egjimenezg@gmail.com>
2017-02-16 09:05:06 -0600

    #162 ajustando prueba

--- core/src/test/groovy/com/modulus/uno/MachineryServiceSpec.groovy
+++ core/src/test/groovy/com/modulus/uno/MachineryServiceSpec.groovy
@@ -6,12 +6,13 @@ import java.lang.Void as Should
 import grails.test.mixin.Mock
 
 @TestFor(MachineryService)
-@Mock([PurchaseOrder,State,Machinery])
+@Mock([PurchaseOrder,State,Machinery,MachineryLink])
 class MachineryServiceSpec extends Specification {
 
   Should "create the link between the state machine and an instance"(){
     given:"the instance"
       PurchaseOrder instance = new PurchaseOrder()
+      instance.save(validate:false)
     and:"the machinery"
       State initialState = new State()
       initialState.save()
