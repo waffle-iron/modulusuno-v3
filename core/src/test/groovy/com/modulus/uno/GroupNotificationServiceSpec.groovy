06ad00d444fc71f70aa27cf3e221ee47563499a0
carlogilmar <carlogilmar12@gmail.com>
2017-02-16 16:59:10 -0600

    #166 importando mock a prueba de GroupNotificationService

--- core/src/test/groovy/com/modulus/uno/GroupNotificationServiceSpec.groovy
+++ core/src/test/groovy/com/modulus/uno/GroupNotificationServiceSpec.groovy
@@ -2,6 +2,7 @@ package com.modulus.uno
 
 import grails.test.mixin.TestFor
 import spock.lang.Specification
+import grails.test.mixin.Mock
 
 /**
  * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
