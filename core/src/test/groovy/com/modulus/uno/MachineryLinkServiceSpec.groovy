56968f7d8edb22c8fe758cdd25607f2ff1082f7d
egjimenezg <egjimenezg@gmail.com>
2017-02-16 15:39:43 -0600

    #162 agregando prueba para verificar la creación de un link entre una máquina de estados y una instancia

---
+++ core/src/test/groovy/com/modulus/uno/MachineryLinkServiceSpec.groovy
@@ -0,0 +1,42 @@
+package com.modulus.uno
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+import java.lang.Void as Should
+import grails.test.mixin.Mock
+import spock.lang.FailsWith
+
+@TestFor(MachineryLinkService)
+@Mock([PurchaseOrder,Bank,State,Machinery,MachineryLink])
+class MachineryLinkServiceSpec extends Specification {
+
+  Should "create the link between the state machine and an instance"(){
+    given:"the instance"
+      PurchaseOrder instance = new PurchaseOrder()
+      instance.save(validate:false)
+    and:"the machinery"
+      State initialState = new State()
+      initialState.save()
+      Machinery machinery = new Machinery(initialState:initialState)
+    when:
+      MachineryLink machineryLink = service.createMachineriesForThisInstance(instance,[machinery])
+    then:
+      machineryLink.id
+      machineryLink.type == PurchaseOrder.class.simpleName
+      machineryLink.machineries
+      machineryLink.machineries[0].id == machinery.id
+  }
+
+  @FailsWith(RuntimeException)
+  Should "fail while trying to create a machine for an instace without the implements"(){
+    given:
+      Bank instance = new Bank()
+      instance.save()
+      Machinery machinery = new Machinery()
+    when:
+      MachineryLink machineryLink = service.createMachineriesForThisInstance(instance,[machinery])
+    then:
+      !machineryLink.id
+  }
+
+}
