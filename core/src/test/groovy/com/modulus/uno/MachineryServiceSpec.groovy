f6c150f47c5d1124ed7ff369cf089bf773dcc11d
egjimenezg <egjimenezg@gmail.com>
2017-02-16 13:15:31 -0600

    coding #162 corrigiendo las pruebas de unidad para la maquina de una entidad

--- core/src/test/groovy/com/modulus/uno/MachineryServiceSpec.groovy
+++ core/src/test/groovy/com/modulus/uno/MachineryServiceSpec.groovy
@@ -1,42 +1,41 @@
 package com.modulus.uno
 
 import grails.test.mixin.TestFor
+import grails.test.mixin.Mock
 import spock.lang.Specification
 import java.lang.Void as Should
-import grails.test.mixin.Mock
-import spock.lang.FailsWith
 
 @TestFor(MachineryService)
-@Mock([PurchaseOrder,Bank,State,Machinery,MachineryLink])
+@Mock([Machinery,State,Transition,Action])
 class MachineryServiceSpec extends Specification {
 
-  Should "create the link between the state machine and an instance"(){
-    given:"the instance"
-      PurchaseOrder instance = new PurchaseOrder()
-      instance.save(validate:false)
-    and:"the machinery"
-      State initialState = new State()
-      initialState.save()
-      Machinery machinery = new Machinery(initialState:initialState)
+  Should "create a new machinary with an action"(){
+    given:"the action"
+      Action action = new Action(name:"Do Something")
+      action.save()
     when:
-      MachineryLink machineryLink = service.createMachineriesForThisInstance(instance,[machinery])
+      Machinery machinery = service.createMachineWithAction(action)
     then:
-      machineryLink.id
-      machineryLink.type == PurchaseOrder.class.simpleName
-      machineryLink.machineries
-      machineryLink.machineries[0].id == machinery.id
+      machinery.initialState
+      machinery.initialState.transitions.size() == 1
+      machinery.initialState.transitions.first().id 
+      machinery.states.size() == 1
   }
 
-  @FailsWith(RuntimeException)
-  Should "fail while trying to create a machine for an instace without the implements"(){
-    given:
-      def instance = new Bank()
-      instance.save()
-      Machinery machinery = new Machinery()
+  Should "add a transition for a machine"(){
+    given:"the machine"
+      Action action = new Action(name:"Do Something")
+      action.save()
+      Machinery machinery = service.createMachineWithAction(action)
+    and: "the second action"
+      Action anotherAction = new Action(name:"Do another thing")
+      anotherAction.save()
     when:
-      MachineryLink machineryLink = service.createMachineriesForThisInstance(instance,[machinery])
+      service.addTransitionToMachinery(machinery.id,action.id,anotherAction.id)
     then:
-      !machineryLink.id
+      machinery.states.size() == 2
+      machinery.states.last().finaState == true
+      machinery.states.first().transitions.size() == 1 
   }
-    
+
 }
