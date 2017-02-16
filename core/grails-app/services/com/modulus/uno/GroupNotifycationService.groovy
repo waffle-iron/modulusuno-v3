9c263bd0843b45ea77ccc153162ba2545e2f88d9
carlogilmar <carlogilmar12@gmail.com>
2017-02-16 16:40:37 -0600

    #166 Implementando funciones de prueba

--- core/grails-app/services/com/modulus/uno/GroupNotifycationService.groovy
+++ core/grails-app/services/com/modulus/uno/GroupNotifycationService.groovy
@@ -5,7 +5,15 @@ import grails.transaction.Transactional
 @Transactional
 class GroupNotifycationService {
 
-    def serviceMethod() {
+  def createNewGroup(def id, ArrayList<User>){
+    println "Creando notification group"
+  }
 
-    }
+  def updateUsersGroup(def groupId, ArrayList<User>){
+    println "Actualizando lista de usuarios de un notification group"
+  }
+
+  def updateNotifyId(def groupId, ArrayList<User>){
+    println "Actualizando idNotify de un notification group exitstentw"
+   }
 }
