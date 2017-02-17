a23217d973faa81047c92c2e98f09a63efca4ce5
egjimenezg <egjimenezg@gmail.com>
2017-02-17 15:57:43 -0600

    coding #165 actualizando implementacion y nombre del método

--- core/grails-app/services/com/modulus/uno/MachineryLinkService.groovy
+++ core/grails-app/services/com/modulus/uno/MachineryLinkService.groovy
@@ -5,17 +5,14 @@ import grails.transaction.Transactional
 @Transactional
 class MachineryLinkService {
 
-  MachineryLink createMachineriesForThisInstance(def instance,ArrayList<Machinery> machineries){
+  MachineryLink createMachineryLinkForThisInstance(def instance,Machinery machinery){
     if(!StateMachine.class.isAssignableFrom(instance.class)){
       throw new RuntimeException("State Machine is not assignable from ${instance.class.simpleName}")
     }
     
     MachineryLink machineryLink = MachineryLink.findByMachineryRefAndType(instance.id,instance.class.simpleName) ?: new MachineryLink(machineryRef:instance.id,
                                                                                                                                       type:instance.class.simpleName)
-    machineries.each{ machinery ->
-      machineryLink.addToMachineries(machinery)
-    }
-
+    machineryLink.machinery = machinery
     machineryLink.save()
     machineryLink
   }
