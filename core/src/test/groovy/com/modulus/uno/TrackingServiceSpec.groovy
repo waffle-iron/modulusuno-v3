904db2a512991e98e606b2f6ba23f738db5ecb43
egjimenezg <egjimenezg@gmail.com>
2017-02-17 11:26:13 -0600

    #165 corrigiendo prueba unitaria

--- core/src/test/groovy/com/modulus/uno/TrackingServiceSpec.groovy
+++ core/src/test/groovy/com/modulus/uno/TrackingServiceSpec.groovy
@@ -7,7 +7,7 @@ import java.lang.Void as Should
 import spock.lang.FailsWith
 
 @TestFor(TrackingService)
-@Mock([PurchaseOrder,Bank,TrackingLogLink,TrackingLog])
+@Mock([PurchaseOrder,Bank,TrackingLogLink,TrackingLog,State])
 class TrackingServiceSpec extends Specification {
 
   Should "create a tracking link for an instance"(){
@@ -26,9 +26,25 @@ class TrackingServiceSpec extends Specification {
     given:
       Bank instance = new Bank()
     when:
-      TrackingLogLink trackingLogLink = service.createTrackinForThisInstance(bank) 
+      TrackingLogLink trackingLogLink = service.createTrackinForThisInstance(instance) 
     then:
       !trackingLogLink.id
+  }
+
+  Should "create the log register for instance"(){
+    given:"the instance"
+      PurchaseOrder purchaseOrder = new PurchaseOrder()
+      purchaseOrder.save(validate:false)
+    and:"its tracking log link"
+      TrackingLogLink trackingLogLink = service.createTrackinForThisInstance(purchaseOrder) 
+    and:"the current state of the instance in the machine"
+      State state = new State()
+      state.save()
+    when:
+      TrackingLog record = service.addRecordToInstanceLog(purchaseOrder,state.id)
+    then:
+      record.id
+      record.state.id == state.id
   }
 
 }
