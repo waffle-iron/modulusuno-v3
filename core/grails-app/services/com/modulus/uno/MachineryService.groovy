65b0e4d38485d137a6601824ae03d38a75ce1f7a
egjimenezg <egjimenezg@gmail.com>
2017-02-17 09:01:44 -0600

    #162 agregando metodo para añadir una transición a la máquina

--- core/grails-app/services/com/modulus/uno/MachineryService.groovy
+++ core/grails-app/services/com/modulus/uno/MachineryService.groovy
@@ -4,20 +4,55 @@ import grails.transaction.Transactional
 
 @Transactional
 class MachineryService {
+ 
+  Machinery createMachineWithAction(Action action){
+    State initialState = new State()
+    State finalState = new State(isFinalState:true)
+    Machinery machinery = new Machinery(initialState:initialState)
+    machinery.addToStates(initialState)
+    machinery.addToStates(finalState)
+    machinery.save()
+    Transition transition = new Transition(action:action,
+                                           stateTo:finalState)
 
-  MachineryLink createMachineriesForThisInstance(def instance,ArrayList<Machinery> machineries){
-    if(!StateMachine.class.isAssignableFrom(instance.class)){
-      throw new RuntimeException("State Machine is not assignable from ${instance.class.simpleName}")
-    }
-    
-    MachineryLink machineryLink = MachineryLink.findByMachineryRefAndType(instance.id,instance.class.simpleName) ?: new MachineryLink(machineryRef:instance.id,
-                                                                                                                                      type:instance.class.simpleName)
-    machineries.each{ machinery ->
-      machineryLink.addToMachineries(machinery)
+    initialState.addToTransitions(transition)
+    initialState.save()
+    machinery
+  }
+
+  Machinery createTransition(Long machineryId,Long actionBeforeId,Long newActionId){
+    Machinery currentMachinery = Machinery.get(machineryId)
+    Action newAction = Action.get(newActionId)
+
+    def criteria = Transition.createCriteria()
+
+    Transition transition = criteria.get {
+      action{
+        eq("id",actionBeforeId)
+      }
+
+      state{
+        machinery{
+          eq("id",machineryId)
+        }
+      }
     }
 
-    machineryLink.save()
-    machineryLink
+    State newState = addTransitionToState(transition.stateTo.id,newAction)
+    currentMachinery.addToStates(newState)
+    currentMachinery.save()
+    currentMachinery
+  }
+
+  State addTransitionToState(Long originStateId,Action action){
+    State state = State.get(originStateId)
+    state.isFinalState = false
+    State newState = new State(isFinalState:true)
+    Transition newTransition = new Transition(action:action,
+                                              stateTo:newState,
+                                              state:state)
+    newTransition.save()
+    newState
   }
 
 }
