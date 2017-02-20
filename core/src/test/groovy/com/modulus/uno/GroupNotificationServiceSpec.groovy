8dbdc893a62895e9aa2c07460a97ad139f0b59cb
carlogilmar <carlogilmar12@gmail.com>
2017-02-20 11:09:27 -0600

    Adding group name attribute to GroupNotification

--- core/src/test/groovy/com/modulus/uno/GroupNotificationServiceSpec.groovy
+++ core/src/test/groovy/com/modulus/uno/GroupNotificationServiceSpec.groovy
@@ -22,12 +22,15 @@ class GroupNotificationServiceSpec extends Specification {
         def user0= new User(username:"User0", enabled:true,
         profile:new Profile(name:"User0", email:"user0@me.com")).save(validate:false)
         ArrayList<User> userList = [user1, user2, user3]
-      and:"a notification id"
+      and:"a notification id and a group name"
         def notificationId = "586d4944e1d4ae54524dd622"
+        def groupName = "Contadores"
       when:"we want to save the group"
-        def firstUserNotificationGroup = service.createGroup(notificationId, userList)
+        def firstUserNotificationGroup = service.createGroup(groupName, notificationId, userList)
       then:"we should get"
         firstUserNotificationGroup.id == 1
+        firstUserNotificationGroup.name == "Contadores"
+        firstUserNotificationGroup.name != "Null-Contadores"
         firstUserNotificationGroup.users == [user1, user2, user3]
         firstUserNotificationGroup.users != [user1, user2, user0]
         firstUserNotificationGroup.notificationId == "586d4944e1d4ae54524dd622"
@@ -44,12 +47,14 @@ class GroupNotificationServiceSpec extends Specification {
         def user6= new User(username:"newUser3", enabled:true,
         profile:new Profile(name:"newUser3", email:"user3new@me.com")).save(validate:false)
         ArrayList<User> newUserList = [user4, user5, user6]
-      and:"a  new notification id"
+      and:"a  new notification id and a new name"
         def newNotificationId = "586d4944e1d4ae5diamon666"
+        def newName = "ContadoresGroup"
       when:"we want to update the userList and the notificationId"
-        service.updateGroupNotification(firstGroup.id, newUserList, newNotificationId)
+        service.updateGroupNotification(firstGroup.id, newName, newUserList, newNotificationId)
       then:"we should get"
         firstGroup.users == newUserList
+        firstGroup.name == "ContadoresGroup"
         firstGroup.notificationId == newNotificationId
     }
 
@@ -70,7 +75,7 @@ class GroupNotificationServiceSpec extends Specification {
         def user3= new User(username:"User3", enabled:true,
         profile:new Profile(name:"User3", email:"user3@me.com")).save(validate:false)
         ArrayList<User> userList = [user1, user2, user3]
-        def firstNotificationGroup = new GroupNotification(notificationId: "763gytdg327fgfg67fv5f", users: userList)
+        def firstNotificationGroup = new GroupNotification(notificationId: "763gytdg327fgfg67fv5f", users: userList, name:"ModulusUnoGroup")
         firstNotificationGroup.save(validate:false)
         firstNotificationGroup
 
