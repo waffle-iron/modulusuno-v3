0a2abaec4f8b943836909a51874d6eca71966aab
egjimenezg <egjimenezg@gmail.com>
2017-02-21 11:29:51 -0600

    #165 agregando nueva prueba para verificar el cambio de estado en la máquina

--- core/src/test/groovy/com/modulus/uno/MachineryServiceSpec.groovy
+++ core/src/test/groovy/com/modulus/uno/MachineryServiceSpec.groovy
@@ -3,13 +3,16 @@ package com.modulus.uno
 import grails.test.mixin.TestFor
 import grails.test.mixin.Mock
 import spock.lang.Specification
+import spock.lang.Ignore
 import java.lang.Void as Should
 
 @TestFor(MachineryService)
-@Mock([Machinery,State,Transition,Action])
+@Mock([PurchaseOrder,Machinery,State,Transition,Action,MachineryLink,LogRecord])
 class MachineryServiceSpec extends Specification {
 
-  Should "create a new machinary with an action"(){
+  
+  
+  Should "create a new machinery with an action"(){
     given:"the action"
       Action action = new Action(name:"Do Something")
       action.save()
@@ -34,8 +37,49 @@ class MachineryServiceSpec extends Specification {
       Machinery machineryUpdated = service.createTransition(machinery.id,action.id,anotherAction.id)
     then:
       machineryUpdated.states.size() == 3
-      machineryUpdated.states.last().isFinalState == true
+      machineryUpdated.states.last().finalState == true
       machineryUpdated.states[1].transitions.size() == 1
+  }
+
+  Should "move the instance to the first state"(){
+    given:"the machinery"
+      PurchaseOrder instance = new PurchaseOrder()
+      instance.save(validate:false)
+    and:"the machinery"
+      Machinery machinery = createMachinery()
+    and:"the link between the instance and its machine"
+      MachineryLink machineryLink = new MachineryLink(machineryRef:instance.id,
+                                                      type:instance.class.simpleName)
+      machineryLink.machinery = machinery
+      machineryLink.save()
+    and:"the action"
+      Action action = Action.findByName("Initial Action")
+      action.save()
+    and:"the tracking service mock"
+      TrackingService trackingServiceMock = Mock(TrackingService)
+      service.trackingService = trackingServiceMock
+    when:
+      State newState = service.moveToAction(instance,action)
+    then:
+      1 * trackingServiceMock.addRecordToInstanceLog(_,_)
+      1 * trackingServiceMock.findLastTrackingLogRecord(_)
+  }
+
+  Machinery createMachinery(){
+    Machinery machinery = new Machinery()
+    machinery.save()
+    ArrayList<Action> actions = [new Action(name:"Sell"),new Action(name:"Buy"),new Action(name:"Send")] 
+    actions*.save()
+    Action firstAction = new Action(name:"Initial Action")
+    firstAction.save()
+    machinery = service.createMachineWithAction(firstAction)
+    machinery = service.createTransition(machinery.id,firstAction.id,actions[0].id)
+   
+    (1..(actions.size()-1)).each{ index ->
+      machinery = service.createTransition(machinery.id,actions[index-1].id,actions[index].id)
+    } 
+
+    machinery 
   }
 
 }
