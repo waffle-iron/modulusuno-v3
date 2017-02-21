4af6d16444f682c23863b32df4701b2de5c29bb2
egjimenezg <egjimenezg@gmail.com>
2017-02-21 13:00:33 -0600

    #165 actualizando el nombre de los servicios y las clases de dominio

--- core/grails-app/services/com/modulus/uno/MachineryLinkService.groovy
+++ core/grails-app/services/com/modulus/uno/MachineryLinkService.groovy
@@ -5,14 +5,14 @@ import grails.transaction.Transactional
 @Transactional
 class MachineryLinkService {
 
-  MachineryLink createMachineryLinkForThisInstance(def instance,Machinery machinery){
-    if(!StateMachine.class.isAssignableFrom(instance.class)){
-      throw new RuntimeException("State Machine is not assignable from ${instance.class.simpleName}")
+  MachineryLink createMachineryLinkForThisInstance(def instance,Machine machine){
+    if(!Machinery.class.isAssignableFrom(instance.class)){
+      throw new RuntimeException("Machinery is not assignable from ${instance.class.simpleName}")
     }
-    
+
     MachineryLink machineryLink = MachineryLink.findByMachineryRefAndType(instance.id,instance.class.simpleName) ?: new MachineryLink(machineryRef:instance.id,
                                                                                                                                       type:instance.class.simpleName)
-    machineryLink.machinery = machinery
+    machineryLink.machine = machine
     machineryLink.save()
     machineryLink
   }
