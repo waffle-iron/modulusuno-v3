a379796a7ef286d863e196da00152a58790c6560
egjimenezg <egjimenezg@gmail.com>
2017-02-22 16:57:44 -0600

    #175 agregando nueva prueba para las acciones de una compañia

---
+++ core/grails-app/services/com/modulus/uno/ActionService.groovy
@@ -0,0 +1,13 @@
+package com.modulus.uno
+
+import grails.transaction.Transactional
+
+@Transactional
+class ActionService {
+
+  def addActionToCompany(Action action,Long companyId){
+    Company company = Company.get(companyId)
+    company.addToActions(action)
+    company.save()
+  }
+}
