4e48528076dcb36e9b9fea2d3887cd67e0383a14
carlogilmar <carlogilmar12@gmail.com>
2017-02-24 17:31:36 -0600

    #171 Agregando actions para eliminar y actualizar

--- web/grails-app/controllers/com/modulus/uno/GroupNotificationController.groovy
+++ web/grails-app/controllers/com/modulus/uno/GroupNotificationController.groovy
@@ -6,14 +6,55 @@ class GroupNotificationController {
     def groupNotificationService
     def corporateService
 
+    //ok
     def create() {
-      def emailersStorage = emailerClientService.getEmailerStorage()
-      render (view:"create", model: [emailers : emailersStorage, users:corporateService.findCorporateUsers(session.corporate.id)])
+      def emailerStorage = emailerClientService.getEmailerStorage()
+      render (view:"create", model: [emailers : emailerStorage, users:corporateService.findCorporateUsers(session.corporate.id)])
     }
 
+    //TODO: estoy mandando session.corporate para guardar usuarios, pero solo para fines prácticos, pedir ayuda!!HEEEEEELP
     def save(){
         groupNotificationService.addNewGroup(params, session.corporate.id)
-        render (view:"create")
+        render (view:"show", model: [groups: groupNotificationService.getGroupsList()])
     }
+
+    //TODO: Acomodar vista para mostrar los grupos
+    //ok
+    def show() {
+      render (view:"show", model: [groups: groupNotificationService.getGroupsList()])
+    }
+
+    //TODO: probar: mostrar datos en el formulario
+    def edit(){
+      def groupNotification = groupNotificationService.getGroup(params.id)
+      def emailerStorage = emailerClientService.getEmailerStorage()
+      //render (view:"edit", model: [group: groupNotification, emailers:emailerStorage, users:corporateService.findCorporateUsers(session.corporate.id)])
+      render (view:"edit", model: [group: groupNotification])
+     }
+
+    //TODO: Probar
+    def update(){
+      log.info "------mexican debugger-----"*10
+      log.info params.dump()
+      groupNotificationService.updateGroup(params.id, params.name, params.users, params.notifyId)
+      render (view:"show", model: [groups: groupNotificationService.getGroupsList()])
+    }
+
+    //TODO: probar esto
+    def delete(){
+      log.info "------mexican debugger-----"*10
+      log.info params.id
+      groupNotificationService.deleteGroup(params.id)
+      render (view:"show", model: [groups: groupNotificationService.getGroupsList()])
+    }
+
+    /*
+
+Carlo:
+[ ] No haz revisado que show con el update y el delete.
+[ ] Tienes probelmas con guardar los usuarios desde la vista.
+[ ] En el servicio para guardar estas necesitando el corporate.session pero eso no va.
+
+     */
 
 }
