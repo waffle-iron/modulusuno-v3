a759d3f4dfda94f687161e35486f8723a736506d
temocpalma <temopalmag@gmail.com>
2017-02-24 10:49:25 -0600

    #172 Agregando campo descuento al item de orden de venta

--- core/grails-app/domain/com/modulus/uno/SaleOrderItem.groovy
+++ core/grails-app/domain/com/modulus/uno/SaleOrderItem.groovy
@@ -8,6 +8,7 @@ class SaleOrderItem {
   String name
   BigDecimal quantity = new BigDecimal(0)
   BigDecimal price = new BigDecimal(0)
+  BigDecimal discount = new BigDecimal(0)
   BigDecimal ieps = new BigDecimal(0)
   BigDecimal iva = new BigDecimal(0)
 
@@ -20,13 +21,22 @@ class SaleOrderItem {
     sku blank:false,size:4..50,matches:/^[A-Za-z0-9-]*$/
     name blank:false,size:1..300
     price min:0.0,max:250000000.00
+    discount min:0.0,max:100.00
     ieps min:0.0,max:100.00
     iva min:0.0,max:100.00
     quantity min:0.0
   }
 
+  BigDecimal getAmountDiscount() {
+    this.price * (this.discount/100)
+  }
+
+  BigDecimal getPriceWithDiscount() {
+    this.price - this.getAmountDiscount()
+  }
+
   BigDecimal getAmountIVA(){
-    this.price * (this.iva/100)
+    this.getPriceWithDiscount() * (this.iva/100)
   }
 
   BigDecimal getAppliedIVA() {
@@ -34,7 +44,7 @@ class SaleOrderItem {
   }
 
   BigDecimal getAmountIEPS(){
-    this.price * (this.ieps/100)
+    this.getPriceWithDiscount() * (this.ieps/100)
   }
 
   BigDecimal getAppliedIEPS() {
@@ -42,11 +52,11 @@ class SaleOrderItem {
   }
 
   BigDecimal getAmountWithoutTaxes(){
-    this.quantity * this.price
+    this.quantity * this.getPriceWithDiscount()
   }
 
   BigDecimal getNetPrice(){
-    this.price + this.getAmountIVA() + this.getAmountIEPS()
+    this.getPriceWithDiscount() + this.getAmountIVA() + this.getAmountIEPS()
   }
 
   BigDecimal getNetAmount(){
@@ -60,9 +70,11 @@ class SaleOrderItem {
         sku:m.sku,
         quantity:m.quantity,
         price:m.price,
+        discount:m.discount,
         ieps:m.ieps,
         iva:m.iva,
-        unitType:m.unitType
+        unitType:m.unitType,
+        currencyType:m.currencyType
       ]
     }
   }
