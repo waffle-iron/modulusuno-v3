f0e472e5bf3556d1135784e6e3c412831a3e6845
neodevelop <juan@makingdevs.com>
2017-02-28 09:59:56 -0600

    #180 renombrando plugin y paquete

--- core/src/main/groovy/core/CoreGrailsPlugin.groovy
+++ core/src/main/groovy/core/CoreGrailsPlugin.groovy
@@ -1,27 +1,26 @@
-package plugin
+package core
 
 import grails.plugins.*
 
-class PluginGrailsPlugin extends Plugin {
+class CoreGrailsPlugin extends Plugin {
 
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "3.0.9 > *"
+    def grailsVersion = "3.2.6 > *"
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
         "grails-app/views/error.gsp"
     ]
 
     // TODO Fill in these fields
-    def title = "Plugin" // Headline display name of the plugin
+    def title = "Core" // Headline display name of the plugin
     def author = "Your name"
     def authorEmail = ""
     def description = '''\
 Brief summary/description of the plugin.
 '''
-    def profiles = ['web']
 
     // URL to the plugin's documentation
-    def documentation = "http://grails.org/plugin/plugin"
+    def documentation = "http://grails.org/plugin/core"
 
     // Extra (optional) plugin metadata
 
