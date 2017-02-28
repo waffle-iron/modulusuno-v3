a73b6a7fb3beb51eab7bc69f00e9d013807c8ca5
egjimenezg <egjimenezg@gmail.com>
2017-02-27 18:33:59 -0600

    #179 acutalizando las pruebas para verificar la creación de las máquinas de estados

--- core/src/test/groovy/com/modulus/uno/MachineServiceSpec.groovy
+++ core/src/test/groovy/com/modulus/uno/MachineServiceSpec.groovy
@@ -10,7 +10,7 @@ import java.lang.Void as Should
 @Mock([PurchaseOrder,Machine,State,Transition,Action,MachineryLink,Company,TrackingLog])
 class MachineServiceSpec extends Specification {
 
-  Should "create a new machinery with an action"(){
+  Should "create a new machine with an action"(){
     given:"the action"
       Action action = new Action(name:"Do Something")
       action.save(validate:false)
@@ -18,9 +18,12 @@ class MachineServiceSpec extends Specification {
       Machine machine = service.createMachineWithAction(action)
     then:
       machine.initialState
-      machine.initialState.transitions.size() == 1
-      machine.initialState.transitions.first().id 
+      machine.transitions.size() == 1
+      machine.transitions.first().id 
       machine.states.size() == 2
+      machine.transitions.size() == 1
+      machine.transitions.first().stateFrom.id == 1
+      machine.transitions.first().stateTo.id == 2
   }
 
   Should "add a transition for a machine"(){
@@ -36,18 +39,43 @@ class MachineServiceSpec extends Specification {
     then:
       updatedMachine.states.size() == 3
       updatedMachine.states.last().finalState == true
-      updatedMachine.states[1].transitions.size() == 1
+      updatedMachine.transitions.size() == 2
+  }
+
+  
+  Should "get the current state of the instance"(){
+    given:"the instance"
+      PurchaseOrder instance = new PurchaseOrder()
+      instance.save(validate:false)
+    and:"the machine"
+      createMachine()
+      Machine machine = Machine.get(1)
+    and:"the link between the instance and its machine"
+      MachineryLink machineryLink = new MachineryLink(machineryRef:instance.id,
+                                                      type:instance.class.simpleName)
+      machineryLink.machine = machine
+      machineryLink.save()
+    and:"the movements"
+      ArrayList<Action> actions = [Action.findByName("Initial Action"),Action.findByName("Sell")]
+      actions.each{ action ->
+        service.moveToAction(instance,action)
+      }
+    when:
+      State state = service.getCurrentStateOfInstance(instance)
+    then:
+      state.id == 3 
   }
 
   Should "move the instance to the first state"(){
-    given:"the machine"
+    given:"the instance"
       PurchaseOrder instance = new PurchaseOrder()
       instance.save(validate:false)
     and:"the company"
       Company company = new Company(bussinessName:"MakingDevs")
       company.save(validate:false)
-    and:"the machinery"
-      Machine machine = createMachine()
+    and:"the machine"
+      createMachine()
+      Machine machine = Machine.get(1)
     and:"the link between the instance and its machine"
       MachineryLink machineryLink = new MachineryLink(machineryRef:instance.id,
                                                       type:instance.class.simpleName)
@@ -61,16 +89,15 @@ class MachineServiceSpec extends Specification {
       machineryLink.trackingLogs.size() == 1
   }
 
-  Machine createMachine(){
+  void createMachine(){
     Machine machine = new Machine()
-    machine.save()
     ArrayList<Action> actions = [new Action(name:"Sell"),new Action(name:"Buy"),new Action(name:"Send")] 
     actions*.save(validate:false)
     Action firstAction = new Action(name:"Initial Action")
     firstAction.save(validate:false)
     machine = service.createMachineWithAction(firstAction)
     machine = service.createTransition(machine.id,firstAction.id,actions[0].id)
-   
+
     (1..(actions.size()-1)).each{ index ->
       machine = service.createTransition(machine.id,actions[index-1].id,actions[index].id)
     } 
